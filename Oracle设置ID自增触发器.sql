38c8a1424f9fc6c1944c628712655c06808096ee
WX-T <154199219@qq.com>
2016-02-25 10:33:59 +0800

    Update Oracle设置ID自增触发器.sql

--- Oracle设置ID自增触发器.sql
+++ Oracle设置ID自增触发器.sql
@@ -1,1 +1,16 @@
+CREATE SEQUENCE "SEQ_表名"
+ INCREMENT BY 1
+ MINVALUE 1
+ MAXVALUE 999999999999999999999999999
+ START WITH 1
+ CACHE 10;
+ 
+ create or replace trigger TRIGGER_表名
+  before insert on 表名
+  for each row
+declare
+  -- local variables here
+begin
+  SELECT SEQ_表名.NEXTVAL INTO :NEW.ID FROM DUAL;
+end TRIGGER_表名;
 
