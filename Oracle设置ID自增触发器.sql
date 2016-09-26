50b294479633512fb859d3be5201d445a5e9bff8
Murviet <154199219@qq.com>
2016-09-26 16:57:51 +0800

    Update Oracle设置ID自增触发器.sql

--- Oracle设置ID自增触发器.sql
+++ Oracle设置ID自增触发器.sql
@@ -1,10 +1,11 @@
+-- 设置sequence
 CREATE SEQUENCE "SEQ_表名"
  INCREMENT BY 1
  MINVALUE 1
  MAXVALUE 999999999999999999999999999
  START WITH 1
  CACHE 10;
- 
+ -- 设置触发器
  create or replace trigger TRIGGER_表名
   before insert on 表名
   for each row
