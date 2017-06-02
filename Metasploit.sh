04e8e3241a585ed1ebded53b3551258d68b4c2b8
Murviet <154199219@qq.com>
2017-06-02 18:40:45 +0800

    Update Metasploit.sh

--- Metasploit.sh
+++ Metasploit.sh
@@ -1,5 +1,8 @@
-nmap 扫描端口及服务详情
-nmap -F -sT -v -sV 192.168.7.81 
+> nmap 扫描端口及服务详情
+    nmap -F -sT -v -sV 192.168.7.81 
 
-nmap 扫描漏洞
-nmap --script=vuln 123.59.58.79
+> nmap 扫描漏洞
+    nmap --script=vuln 123.59.58.79
+
+> hrdya 爆破ssh
+    hydra -L users.txt -P password.txt -t 1 -vV -e ns 192.168.1.104 ssh
